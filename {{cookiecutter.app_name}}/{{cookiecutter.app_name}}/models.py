47e704d93db7116fa4b822b2cf9ae353812a4ca1
alphardex <2582347430@qq.com>
2018-12-17 22:05:50 +0800

    add pylint and beautify dropdown

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/models.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/models.py
@@ -12,9 +12,6 @@ class User(db.Model, UserMixin):
     password_hash = db.Column(db.String(128))
     member_since = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return f'<User {self.username}>'
-
     def set_password(self, password):
         self.password_hash = security.generate_password_hash(password)
 
